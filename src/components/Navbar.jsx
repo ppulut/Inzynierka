4317d4eadc331bfccede86b86352cf8f2571efba
Pawel <kielczak1999@gmail.com>
2022-03-26 20:45:58 +0100

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import Badge from '@mui/material/Badge';
+import LoginIcon from '@mui/icons-material/Login';
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
 
 
 const Container = styled.div`
@@ -8,27 +11,54 @@ const Container = styled.div`
 `;
 
 const Wrapper = styled.div`
-    padding: 10px 20px
-    display: flex;
-    justify-content: space-between;
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
+
+
+const Lewo = styled.div`
+felx: 1;
+`;
+
+const Logo = styled.h1`
+font-weight: bold;
+`
+
 const Center = styled.div`
-    
+felx: 1;
 `;
 
-
-const Right = styled.div`
+const Prawo = styled.div`
+felx: 1;
+display: flex;
+align-items: center;
+justify-content: flex-end;
 
 `;
 
+const Menu = styled.div`
+font-size: 14px;
+cursor: pointer;
+margin-left: 20px;
+`
 
 const Navbar = () => {
   return (
     <Container>
         <Wrapper>
-            <Center>center</Center>
-            <Right>prawo</Right>
-            </Wrapper>
+            <Lewo>Lewo</Lewo>
+            <Center><Logo>BB Nails</Logo></Center>
+            <Prawo>
+              <Menu>Rejestracja</Menu>
+              <AccessibilityNewIcon color="action" />
+              <Menu>Zaloguj</Menu>
+              <Badge color="primary">
+      <LoginIcon color="action" />
+              </Badge>
+            </Prawo>
+        </Wrapper>
         </Container>
   )
 }
