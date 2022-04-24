43c653e9f489c20b8e1ba3aa26174aa9a1434602
Pawel <kielczak1999@gmail.com>
2022-04-24 17:44:04 +0200

    commit

---
+++ backend/Nailshop/src/main/java/com/Webpage/Nailshop/NailshopApplication.java
@@ -0,0 +1,13 @@
+package com.Webpage.Nailshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailshopApplication.class, args);
+	}
+
+}
---
+++ backend/Nailshop/src/test/java/com/Webpage/Nailshop/NailshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Webpage.Nailshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
