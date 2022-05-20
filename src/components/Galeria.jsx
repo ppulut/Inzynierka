cb4025f806e5b584e76b35d2fb9049314b0e5510
Pawel <kielczak1999@gmail.com>
2022-05-20 13:00:40 +0200

    commit

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -33,10 +33,10 @@ const Galeria = () => {
           <div class="uls">
           <div class="divs"><img src="https://raw.githubusercontent.com/ppulut/Inzynierka/master/galeria/1.jpg?token=GHSAT0AAAAAABTVTFI4J7TJERWQIT7JESJ6YUHOG3Q"/></div>
           <div class="divs"><img src="https://raw.githubusercontent.com/ppulut/Inzynierka/master/galeria/2.jpg?token=GHSAT0AAAAAABTVTFI5LHL5KNG4QJQ7KTXKYUHOG6A"/></div>
-          <div class="divs"><img src="https://www.new-nails.com/wp-content/uploads/2017/01/kurs-zdobienia-paznokci-barok.jpg"/></div>
-          <div class="divs"><img src="https://www.new-nails.com/wp-content/uploads/2017/01/kurs-zdobienia-paznokci-barok.jpg"/></div>
-          <div class="divs"><img src="https://www.new-nails.com/wp-content/uploads/2017/01/kurs-zdobienia-paznokci-barok.jpg"/></div>
-          <div class="divs"><img src="https://www.new-nails.com/wp-content/uploads/2017/01/kurs-zdobienia-paznokci-barok.jpg"/></div>
+          <div class="divs"><img src="https://raw.githubusercontent.com/ppulut/Inzynierka/master/galeria/3.jpg?token=GHSAT0AAAAAABTVTFI4DBHX4LNQPZ3SIKVUYUHOLDA"/></div>
+          <div class="divs"><img src="https://raw.githubusercontent.com/ppulut/Inzynierka/master/galeria/4.jpg?token=GHSAT0AAAAAABTVTFI4HNEJ2RTUC5JVP6HMYUHOLEQ"/></div>
+          <div class="divs"><img src="https://raw.githubusercontent.com/ppulut/Inzynierka/master/galeria/5.jpg?token=GHSAT0AAAAAABTVTFI4VTZ4G6YTKC6X4AFOYUHOLFQ"/></div>
+          <div class="divs"><img src="https://raw.githubusercontent.com/ppulut/Inzynierka/master/galeria/6.jpg?token=GHSAT0AAAAAABTVTFI46AD5A6DLGUHQMW6WYUHOLGQ"/></div>
           <div class="divs"><img src="https://www.new-nails.com/wp-content/uploads/2017/01/kurs-zdobienia-paznokci-barok.jpg"/></div>
           <div class="divs"><img src="https://www.new-nails.com/wp-content/uploads/2017/01/kurs-zdobienia-paznokci-barok.jpg"/></div>
   <div class="divs"><img src="https://www.new-nails.com/wp-content/uploads/2017/01/kurs-zdobienia-paznokci-barok.jpg"/></div>
