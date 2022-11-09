ef7e047b4a734ac0351d62c3ea6fe4a031aa0084
ppulut <kielczak1999@gmail.com>
2022-11-09 13:48:18 +0100

    commit

--- src/components/NEWS.jsx
+++ src/components/NEWS.jsx
@@ -1,11 +1,42 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 import styled from "styled-components";
 import { mobile } from "../responsive";
+import { Send } from "@mui/icons-material";
 
 const url = "https://wp.us10.list-manage.com/subscribe/post?u=fc5ddd29718ff4112df7e4760&amp;id=4c0275edea&amp;f_id=005337e2f0";
 
 // simplest form (only email)
-const SimpleForm = () => <MailchimpSubscribe url={url}/>
+
+
+const CustomForm = ({ subscribe, status, message, onValidated  }) => {
+  let email;
+  const submit = () =>
+    email &&
+    email.value.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email.value,
+    });
+  return (
+    <Container>
+      {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+        {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
+        {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+      <Title>Newsletter</Title>
+      <Desc>Otrzymuj informacje o nowej ofercie!</Desc>
+      <InputContainer>
+      <Input 
+        placeholder="Twój email"
+        ref={node => (email = node)}
+        type="email"
+        />
+        <Button onClick={submit}>
+        Wyślij <Send/>
+      </Button>
+        </InputContainer>
+    </Container>
+  );
+};
+
 
 const Container = styled.div`
   height: 60vh;
@@ -29,22 +60,42 @@ const Desc = styled.div`
   ${mobile({ textAlign: "center" })}
 `;
 
+const InputContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgray;
+  ${mobile({ width: "100%" })}
+`;
+
+const Input = styled.input`
+  border: none;
+  flex: 8;
+  padding-left: 20px;
+`;
+const Button = styled.button`
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
+`;
 
 // use the render prop and your custom form
 const NEWS = () => (
+  <Container>
   <MailchimpSubscribe
     url={url}
     render={({ subscribe, status, message }) => (
-      <Container>
-         <Title>Newsletter</Title>
-      <Desc>Otrzymuj informacje o nowej ofercie!</Desc>
-        <SimpleForm onSubmitted={formData => subscribe(formData)} />
-        {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
-        {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
-        {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
-      </Container>
+      <CustomForm
+        status={status}
+        message={message}
+        onValidated={formData => subscribe(formData)}
+      />
     )}
   />
+  </Container>
 )
 
 export default NEWS;
